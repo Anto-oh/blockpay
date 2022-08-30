216c8b84f0a2cc4ee51f0136d90d7e8f23cdcce9
Anto-oh <64340817+Anto-oh@users.noreply.github.com>
2022-08-30 15:05:42 -0500

    Update Stake.js
    Ediciones requeridas y composición de info

--- src/Stake.js
+++ src/Stake.js
@@ -1,13 +1,9 @@
-import React from 'react';
-import {  Link } from "react-router-dom";
-import avax1 from './avalanche.png'
+import React, { Component } from 'react'
 import { IconWallet} from '@tabler/icons';
 import 'remixicon/fonts/remixicon.css'
 import { useState } from "react";
-const Stake= () =>{
+function Stake({balanceS, account}){
 
-/*const [balance, setBalance] = useState(props.balance)
-const [account, setAccount] = useState(props.account)*/
 return(
 <div className='Stake'>
     <div style={{paddingLeft:"10%", width:"80%"}}>
@@ -15,47 +11,37 @@ return(
         <div className='balanceTop'>
     <div className='balanceLeft'>
         <div style={{color:"#949494"}}>Balance</div>
-        <div style={{fontWeight:"bold", fontSize:"20px"}}>0.00 AVAX</div>
+        <div style={{fontWeight:"bold", fontSize:"20px"}}>{balanceS} BPAY</div>
         </div>
         <div className='rightWallet'> <IconWallet
     size={28} // set custom `width` and `height`
     color="grey" // set `stroke` color
     stroke={1}  // set `stroke-width`
     strokeLinejoin="miter" // override other SVG props
-  />Account</div>
+  />{account}</div>
     </div>
     </div>
-    <div className="savaxBalance"> 
-    <div style={{color:"#949494"}}>Stake Balance</div>
-    <div className='right' style={{color:"#949494"}}>Apr</div>
-    <div style={{fontWeight:"bold", fontSize:"20px"}}>0.0000 sAVAX</div>
-    <div className='right' style={{fontWeight:"bold", fontSize:"20px"}}>~7.20 %</div>
-    </div></div>
+
+    
+    </div>
 
 
     <div style={{display:"grid", gridTemplateColumns: "auto auto"}}>
     <div className='stakebox'>
-    <p>Stake</p>
+    <p>1 year Stake</p>
     <input className='input' placeholder='0.00'></input>
     <button className='button2'>STAKE</button>
     
-    <div style={{display:"flex", justifyContent:"space-between", width:"80%", fontSize:"13px"}}>
-    <div>You will receive <i class="ri-question-line"></i></div> <div>~0.00 sAVAX</div></div>
-    <div style={{display:"flex", justifyContent:"space-between", width:"80%", fontSize:"13px"}}>
-    <div>Exchange rate <i class="ri-question-line"></i></div> <div>1 AVAX ≈ 0.969645 sAVAX</div></div>
+   
 
     </div>
     
     
     <div className='unstakebox'>
-    <p>Unstake</p>
+    <p>1.5 years Stake</p>
     <input className='input' placeholder='0.00'></input>
-    <button className='button3'>UNSTAKE</button>
-    
-    <div style={{display:"flex", justifyContent:"space-between", width:"80%", fontSize:"13px"}}>
-    <div>You will receive <i class="ri-question-line"></i></div> <div>~0.00 sAVAX</div></div>
-    <div style={{display:"flex", justifyContent:"space-between", width:"80%", fontSize:"13px"}}>
-    <div>Exchange rate <i class="ri-question-line"></i></div> <div>1 AVAX ≈ 0.969645 sAVAX</div></div>
+    <button className='button3'>STAKE</button>
+ 
 
     </div>
     
@@ -64,5 +50,6 @@ return(
 
 )
 }
+
 
 export default Stake
