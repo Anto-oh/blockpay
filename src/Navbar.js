2e6b1d325ef22da9d7702253eab39ae51ef27fe7
Anto-oh <64340817+Anto-oh@users.noreply.github.com>
2022-08-30 15:11:17 -0500

    Update Navbar.js
    Composición

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,43 +2,14 @@ import React, {useState, useEffect} from 'react';
 import {  Link } from "react-router-dom";
 import logo from './logosbpay/Logotipo_morado.png'
 import connectWallet from './connectWallet';
-const Navbar= () =>{
-  const [walletAddress, setwalletAddress] = useState("Connect Wallet")
-
-  const connectWallet = async () => {
-    if (typeof window.ethereum !== 'undefined'){
-        const accounts = await window.ethereum.request({method: 'eth_requestAccounts' })
-
-
-        const abbreviate = (input) => {
-          
-          return input === "Wallet" ? input : [input.substring(0,6), "...", input.substring(input.length-4, input.length)].join(''); 
-      };
-      
-      const result = abbreviate(accounts[0]);
-      
-      console.log(result);
-
-
-        setwalletAddress(result)
-
-        
-
-  }
-}
-
-
-
-
-
-
-
+const Navbar= ({func, address}) =>{
 
   return (
   <div className="navbar" >
  <a><img src={logo} style={{height:"180px"}}></img></a>
  <a>Stake</a>
- <button className='walletButton' onClick={connectWallet}>{walletAddress}</button>
+ <button className='walletButton' onClick={()=> {
+  func()}}>{address}</button>
   </div>
   );
 }
